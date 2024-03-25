6b1a8f0561c0999bba6f00b9a91bb6e560eb3207
Paul PARISOT <paul.parisot@epitech.eu>
2024-03-25 10:21:32 +0100

    fix: delete b and make it working

--- app/Main.hs
+++ app/Main.hs
@@ -13,7 +13,6 @@ import System.Environment
 import System.Exit
 
 import KMeansData.GetConfArgs
-import KMeansData.TrupleData
 import KMeansAlgorithm
 import Options.Applicative
 import GetCentroids
@@ -23,13 +22,13 @@ exitError :: IO()
 exitError = exitWith (ExitFailure 84)
 
 startKMeans :: Conf -> [Info] -> IO()
-startKMeans (Conf cClusters cConvLimit _) infos =
-    (initCentroids infos cClusters) >>= (\cen ->
+startKMeans (Conf cClusters cConvLimit _) infos
+    | length infos < cClusters = exitError
+    | otherwise = (initCentroids infos cClusters) >>= (\cen ->
         executeKMeans cen infos cConvLimit)
 
 handleConf :: Conf -> IO ()
 handleConf (Conf cClusters cConvLimit cFile)
-    | cClusters <= 0 || cClusters >= length b = exitError
     | cConvLimit <= 0 = exitError
     | otherwise = getImageParsing cFile >>= (\pars -> case pars of
         Just res -> startKMeans (Conf cClusters cConvLimit cFile) res
